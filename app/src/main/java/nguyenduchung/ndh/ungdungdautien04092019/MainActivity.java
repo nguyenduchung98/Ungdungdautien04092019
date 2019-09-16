ab3c5aa999884ed5d66c2c657329b1ad751d9dd6
Nguyen Duc Hung <hungrom98x@gmail.com>
2019-09-16 21:10:06 +0700

    Tham chieu tham tri

--- app/src/main/java/nguyenduchung/ndh/ungdungdautien04092019/MainActivity.java
+++ app/src/main/java/nguyenduchung/ndh/ungdungdautien04092019/MainActivity.java
@@ -4,44 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView mtxt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //1 :Array: xem, sua
-        //Khoi tao.
-//        String[] mangTens=new String [5];
-//        String[] mangten={"Hoa","Hue","Hong","Hang"};
-//        //Chinh sua
-//        mangTens[0]="Hoa";
-//        mangTens[1]="Hang";
-//        mangTens[2]="Hue";
-//        mangTens[3]="Lan";
-//        mangTens[4]="Linh";
-//
-//        for (int i=0;i<mangTens.length;i++){
-//            // Ctrl + P :Xem tham so truyen vao
-//            Log.d("BBB",mangTens[i]);
-//        }
-
-        //2 : Araylist
-        //Khoi tao:
-        ArrayList<Integer> mangintegers= new ArrayList<>();
-        //Them gia tri
-        mangintegers.add(1);
-        mangintegers.add(2);
-        mangintegers.add(3);
-        Log.d("BBB",String.valueOf(mangintegers.size()));
-        //Xoa
-        mangintegers.remove(0);
-        Log.d("BBB",String.valueOf(mangintegers.size()));
-        //chinh sua
-        mangintegers.set(0,6);
-        Log.d("BBB",mangintegers.get(0).toString());
+//       //kieu nguyen tbhuy
+//        int a=5;
+//        int b=4;
+//        a=b
+//                a thay doi, b ko thay doi: vi b truyrn tham tri
+//        //kieu object
+//        int a=5;
+//        int b=4;
+//        a=b
+//        a thay doi, b thay doi: vi b truyrn tham chieu
     }
 }
